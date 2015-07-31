eea5399ff513c84af53320674c83fa1bbb23e314
Alex <aleksandernasr@gmail.com>
2015-07-31 14:07:29 +0100

    changed file structure so that angular is accessible, connected unit tests to karma and now writing first test

--- app/js/todoList.js
+++
@@ -1,2 +0,0 @@
-var todoList = angular.module('ToDoList');
-)
--- test/todoController.spec.js
+++ test/todoController.spec.js
@@ -1,16 +1,20 @@
 describe('ToDoListController', function(){
 
-  beforeEach(function(){
-    browser.get('http://localhost:8080')
-  })
+  beforeEach(module('ToDoList'));
+
+  var controller
+
+  beforeEach(inject(function($controller){
+    controller = $controller('ToDoListController');
+  }));
 
   it('has a title on the webpage', function(){
     expect(browser.getTitle()).toEqual('To-do List')
   })
 
-
+  //
   // it('should create "todoList" model with 3 items', function(){
   //
   // })
 
-})
+});
